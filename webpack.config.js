c22bbb8895d70fe35688f69e0ac997ce24643758
YotaArai <yotaarai85@gmail.com>
2021-07-11 21:37:56 +0900

    chore: 出力先ディレクトリ名変更(Github Pages対応)

--- webpack.config.js
+++ webpack.config.js
@@ -8,7 +8,7 @@ module.exports = {
   },
   devtool: "source-map",
   output: {
-    path: path.resolve(__dirname, "dist"),
+    path: path.resolve(__dirname, "docs"),
     filename: "[name]/main.js"
   },
   target: 'node',
